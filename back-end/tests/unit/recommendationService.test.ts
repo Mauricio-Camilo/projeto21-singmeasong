bc7410bb414db0f466952b92f5aea7e65a6a4615
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-07-29 10:28:06 -0500

    feat: add unit test for downvote function

--- back-end/tests/unit/recommendationService.test.ts
+++ back-end/tests/unit/recommendationService.test.ts
@@ -1,6 +1,7 @@
 import { jest } from "@jest/globals";
 import { CreateRecommendationData, recommendationService } from "../../src/services/recommendationsService.js";
 import { recommendationRepository } from "../../src/repositories/recommendationRepository.js";
+import { Recommendation } from "@prisma/client";
 
 describe("recommendation service test suite", () => {
 
@@ -72,16 +73,39 @@ describe("recommendation service test suite", () => {
             return null
         });
 
-        // jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(() : any => {
-        //     return recommendation
-        // });
-
         const promise =  recommendationService.upvote(id);
 
         expect(promise).rejects.toEqual({
             message: "",
             type: "not_found"
         });
+
+    })
+
+    it("should validate downvote function", async () => {
+        const recommendation : Recommendation = {
+            id: 1,
+            name: "Song name",
+            youtubeLink: "https://www.youtube.com/watch?v=chwyjJbcs1Y",
+            score: -10
+        }
+
+        jest.spyOn(recommendationRepository, 'find').mockImplementationOnce(() : any => {
+            return recommendation
+        });
+
+        jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(() : any => {
+            return recommendation
+        });
+
+        jest.spyOn(recommendationRepository, 'remove').mockImplementationOnce(() : any => {});
+
+
+        await recommendationService.downvote(recommendation.id);
+
+        expect(recommendationRepository.find).toBeCalled();
+        expect(recommendationRepository.updateScore).toBeCalled();
+        expect(recommendationRepository.remove).toBeCalled();
 
     })
 
